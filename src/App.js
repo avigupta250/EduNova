43299f716e04c7154397caa1c0fe354bd0febdb5
avigupta250 <avinashkumarnav52@gmail.com>
2024-03-07 20:21:04 +0530

    basic react setup done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-red-500">
+     <h1 className="">Hola avi</h1>
+    </div>
+  );
+}
+
+export default App;
